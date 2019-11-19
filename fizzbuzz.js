bae4ce8330172f0c13cdff9e2d09fa3326f3bfe8
C0NEILL <57679647+C0NEILL@users.noreply.github.com>
2019-11-19 20:17:18 +0000

    'fizzbuzz' v2

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,20 +1,21 @@
 
 // More Information about this assisngment is available in the README.md file
 
-for( var i = 1; i <= 100; i++ )
+for( var i = 1; i <= 100; i++ ){
 
-    if(i % 3 == 0 && i % 5 == 0)(
+    if(i % 3 == 0 && i % 5 == 0){
     output("fizzbuzz");
-)
+    }
     
-    else if ( i % 5 == 0 )(
+    else if ( i % 5 == 0 ){
         output("buzz");
-    )
+    }
 
-    else if( i % 3 == 0)(
+    else if( i % 3 == 0){
         output( "fizz" );
-        )
+}
 
-    else(
+    else{
         output( i );
-    )
+    }
+}
